5f9dfede34b3ef2c854d18224b37dc201dc636bb
nirmalsubashanaD <s225105848@deakin.edu.au>
2025-05-23 20:10:34 +1000

    init

--- task-manager/app/index.js
+++ task-manager/app/index.js
@@ -7,6 +7,10 @@ app.get('/health', (req, res) => {
   res.status(200).send('Healthy!');
 });
 
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).end(); // No content for favicon
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
